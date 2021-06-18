e4916d4a29e9bc9d9fc5425a1ee4b64e2bf9040e
Aman Taneja <amantaneja23@gmail.com>
2021-06-18 18:50:52 +0200

    add more tests

--- test/api_test.py
+++ test/api_test.py
@@ -8,15 +8,17 @@ client = app.test_client()
 
 request_schema = {
 	'isin': 'amantaneja23',
-	'limit_price': 23,
+	'limit_price': 23.03,
 	'side': 'sell',
 	'valid_until': 1623530303511,
 	'quantity': 69
 }
+# copy() because it is a flat dict, no need for deepcopy()
 
 
 def test_valid_post_orders():
-	return_code, response = post_orders_request(request_schema)
+	request = request_schema.copy()
+	return_code, response = post_orders_request(request)
 	print(str(response))
 	assert return_code == 201
 	assert response is not None
@@ -26,27 +28,102 @@ def test_valid_post_orders():
 
 
 def test_invalid_isin_not_string():
-	request_schema['isin'] = 'amantaneja'
-	return_code, response = post_orders_request(request_schema)
+	request = request_schema.copy()
+	request['isin'] = False
+	return_code, response = post_orders_request(request)
 	print(str(response))
 	assert return_code == 400
 	assert 'isin' in response['validation_errors']
 
 
 def test_invalid_isin_short_string():
-	request_schema['isin'] = 'amantaneja'
-	return_code, response = post_orders_request(request_schema)
+	request = request_schema.copy()
+	request['isin'] = 'amantaneja'
+	return_code, response = post_orders_request(request)
 	print(str(response))
 	assert return_code == 400
 	assert 'isin' in response['validation_errors']
 
 
 def test_invalid_isin_long_string():
-	request_schema['isin'] = 'amantaneja2303'
-	return_code, response = post_orders_request(request_schema)
+	request = request_schema.copy()
+	request['isin'] = 'amantaneja2303'
+	return_code, response = post_orders_request(request)
 	print(str(response))
 	assert return_code == 400
 	assert 'isin' in response['validation_errors']
+
+
+def test_invalid_limit_price_not_float():
+	request = request_schema.copy()
+	request['limit_price'] = "float"
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'limit_price' in response['validation_errors']
+
+
+def test_invalid_limit_price_negative_float():
+	request = request_schema.copy()
+	request['limit_price'] = -23.03
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'limit_price' in response['validation_errors']
+
+
+def test_invalid_side_not_string():
+	request = request_schema.copy()
+	request['side'] = 0
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'side' in response['validation_errors']
+
+
+def test_invalid_side_not_valid():
+	request = request_schema.copy()
+	request['side'] = "gamble"
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'side' in response['validation_errors']
+
+
+def test_invalid_valid_until_not_integer():
+	request = request_schema.copy()
+	request['valid_until'] = "integer"
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'valid_until' in response['validation_errors']
+
+
+def test_invalid_valid_until_negative_integer():
+	request = request_schema.copy()
+	request['valid_until'] = -420
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'valid_until' in response['validation_errors']
+
+
+def test_invalid_quantity_not_integer():
+	request = request_schema.copy()
+	request['valid_until'] = "long"
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'valid_until' in response['validation_errors']
+
+
+def test_invalid_quantity_negative_integer():
+	request = request_schema.copy()
+	request['quantity'] = -69
+	return_code, response = post_orders_request(request)
+	print(str(response))
+	assert return_code == 400
+	assert 'quantity' in response['validation_errors']
 
 
 def post_orders_request(request):
